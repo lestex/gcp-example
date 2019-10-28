252ba4ad8a47edab2f7d7d73126bd888847880cc
Andrey Larin <lestex@gmail.com>
2019-10-28 13:54:40 -0400

    comments out main state

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "gcs" {
-    bucket = "terraform-248411-state-test"
-    prefix = "terraform/state"
-  }
-  required_version = "~> 0.12.0"
-}
+# terraform {
+#   backend "gcs" {
+#     bucket = "terraform-248411-state-test"
+#     prefix = "terraform/state"
+#   }
+#   required_version = "~> 0.12.0"
+# }
